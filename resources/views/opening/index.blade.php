9ac4b2be3890e9c3913a663df4d4249533fb22d3
danyelcid <49032982+danyelcid@users.noreply.github.com>
2024-07-31 17:49:35 -0300

    creating Breadcrumbs component and using it on index and show views

--- resources/views/opening/index.blade.php
+++ resources/views/opening/index.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4"
+           :links="['Openings' => route('openings.index')]" />
     @foreach($openings as $opening)
         <x-opening-card class="mb-4" :$opening>
-
             <div>
                 <x-link-button :href="route('openings.show', $opening)">
                     Show
                 </x-link-button>
             </div>
         </x-opening-card>
-
     @endforeach
 </x-layout>
--- resources/views/opening/show.blade.php
+++ resources/views/opening/show.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4"
+       :links="['Openings' => route('openings.index'), $opening->title => '#']" />
     <x-opening-card :$opening/>
 </x-layout>
